ba427da03e05f948c4158f260a42f15a00e1bedf
Pol7 <Paolo@paolos-mbp.homenet.telecomitalia.it>
2014-03-08 13:10:40 +0100

    prova pagina vuota

--- cgi-bin/creaPagina.pl
+++ cgi-bin/creaPagina.pl
@@ -2,11 +2,12 @@
 # Script che crea un form HTML
 use CGI;
 use CGI::Carp qw(fatalsToBrowser);
-#die "Fatal errors are now sent to browser";
+
 $pagina = new CGI; 
-$value=$pagina->header('text/html');
-$value = $value.$pagina->start_html;
-print $value;
-print $pagina->h1("Questo &egrave; il mio titolo");
+print $pagina->header('text/html');
+print $pagina->start_html;
+
+
 print '<p>paragrafo</p>';
+
 print $pagina->end_html;
