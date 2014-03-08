0b12b35f4aab8b37cdf823439462bcb382af6ace
Pol7 <Paolo@paolos-mbp.homenet.telecomitalia.it>
2014-03-08 12:23:25 +0100

    provo creazione pagina semplice

--- cgi-bin/creaPagina.pl
+++ cgi-bin/creaPagina.pl
@@ -9,13 +9,9 @@ $pagina = new CGI;
 print  '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>Primi</title>
-		<link href="./css/page_style.css" rel="stylesheet" type="text/css" media="screen"/>
-		<link href="./css/page_styleMedium.css" rel="stylesheet" type="text/css" media="handheld, screen and (max-width:1320px), only screen and (max-device-width:1320px)" />
-		<link href="./css/page_styleSmall.css" rel="stylesheet" type="text/css" media="handheld, screen and (max-width:690px), only screen and (max-device-width:690px)" />
-	</head>
+		</head>
 	<body>';
-#print $pagina->h1("Questo &egrave; il mio titolo");
 print '<p>paragrafo</p>';
-print $pagina->end_html;
+print '</body>
+</html>
