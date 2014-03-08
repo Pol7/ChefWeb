6bb533613f90a73e9a091ab00a2c8b31c4eed863
Pol7 <Paolo@paolos-mbp.homenet.telecomitalia.it>
2014-03-08 12:36:35 +0100

    PAGINA

--- cgi-bin/creaPagina.pl
+++ cgi-bin/creaPagina.pl
@@ -3,15 +3,9 @@
 use CGI;
 use CGI::Carp qw(fatalsToBrowser);
 #die "Fatal errors are now sent to browser";
-#$pagina = new CGI; 
-#print $pagina->header('text/html');
-#$value = $pagina->start_html; 
-print  '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
-	<head>
-		<title>Primi</title>
-		</head>
-	<body>';
+$pagina = new CGI; 
+print $pagina->header('text/html');
+$value = $pagina->start_html; 
+#print $pagina->h1("Questo &egrave; il mio titolo");
 print '<p>paragrafo</p>';
-print '</body>
-</html>';
+print $pagina->end_html;
