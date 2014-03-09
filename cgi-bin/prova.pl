542255e8fd2ecd514570916a90a0fce0a8608696
Pol7 <Paolo@paolos-mbp.homenet.telecomitalia.it>
2014-03-09 10:55:16 +0100

    pagina di prova

---
+++ cgi-bin/prova.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+# Script che crea un form HTML
+use CGI;
+use CGI::Carp qw(fatalsToBrowser);
+
+$pagina = new CGI; 
+print $pagina->header('text/html');
+print $pagina->start_html(
+				-title=>'prova',
+				-style=>{'src'=>'../public_html/css/page_style.css'},
+				-lang=>'it',
+		);
+print '<form action=“elabora.cgi” method=“get”>
+		Tecnologie Web - 31
+		Tecnologie Web - 32
+	   </form>'
+print $pagine->end_html;
