7596418a32794a30160fb792a2c2aea5f64ff5f0
Calu92 <luca.favaretto@hotmail.it>
2014-03-10 15:29:37 +0100

    prova

--- cgi-bin/inserimento.pl
+++ cgi-bin/inserimento.pl
@@ -1,5 +1,46 @@
 #!/usr/bin/perl
-# Script che crea un form HTML
+# Script che inserisce i dati nell'XML
 use CGI;
 use CGI::Carp qw(fatalsToBrowser);
 use XML::LibXML;
+$pagina = new CGI;
+
+my $file = 'ricette.xml';
+#creazione oggetto parser
+my $parser = XML::LibXML->new();
+#apertura file e lettura input
+my $doc = $parser->parse_file($file) || die("Operazioni di parsing fallita");
+#recupero l'elemento radice
+my $root = $doc->getDocumentElement || die("Non accedo alla radice");
+
+#recupero input della form
+$in = $ENV{'QUERY_STRING'};
+
+$nome= $pagina->param('nomeRicetta');
+print $nome;
+
+
+
+
+
+
+#creo una stringa con un nuovo elemento
+$nuovo_el = "\n<ricetta> $input{'contenuto'}</ricetta>\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
