8abc0f284ce135c1d6654224728bc6b503777e32
Calu92 <luca.favaretto@hotmail.it>
2014-03-10 17:43:56 +0100

    Update Inserimento.pl

--- cgi-bin/Inserimento.pl
+++ cgi-bin/Inserimento.pl
@@ -19,8 +19,16 @@ my $root = $doc->getDocumentElement || die("Non accedo alla radice");
 #recupero input della form
 my $in = $ENV{'QUERY_STRING'};
 
-my $nome= $pagina->param('nomeRicetta');
-print $nome;
+my $nome = $pagina->param('nomeRicetta');
+my $tipo = $pagina->param('tipologia');
+my $autore = $pagina->param('nomeAutore');
+my $img = $pagina->param('immagine');
+my $proc = $pagina->param('message');
+my $nome = $pagina->param('nomeRicetta');
+
+$new_nodo_string = "<ricetta tipo='$tipo'>\n<nome>$nome</nome>\n<autore>$autore</autore>\n<img></img>\n<>\n</comments>\n</episode>\n";
+$new_nodo = $parser->parse_balanced_chunk($new_nodo_string);
+$season_to_process->appendChild($new_nodo);
 
 
 
