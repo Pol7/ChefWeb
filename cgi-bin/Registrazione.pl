13b11f0febcb2e20bb8e935a38acd63215f9102b
Anthony <signorianthony@gmail.com>
2014-03-10 23:05:36 +0100

    Update Registrazione.pl

--- cgi-bin/Registrazione.pl
+++ cgi-bin/Registrazione.pl
@@ -18,7 +18,7 @@ my $root = $doc->getDocumentElement || die("Non accedo alla radice");
 my @utenti = $root->getElementsByTagName("utenti");
 
 #recupero input della form
-my $in = $ENV{'CONTENT_LENGTH'};
+my $in = $ENV{'QUERY_STRING'};
 
 my $username = $pagina->param('username');
 my $password = $pagina->param('password');
@@ -30,7 +30,7 @@ my $mese = $pagina->param('mese');
 my $anno = $pagina->param('anno');
 
 
-my $new_nodo_string = "<utente>\n  <username>$username</username>\n<password>$password</password>\n<nome>$nome</nome>\n<cognome>$cognome</cognome>\n<sesso>$sesso</sesso>\n<giorno>$giorno</giorno>\n<mese>$mese</mese>\n<anno>$anno</anno>\n</utente>";
+my $new_nodo_string = "\n<utente>\n<username>$username</username>\n<password>$password</password>\n<nome>$nome</nome>\n<cognome>$cognome</cognome>\n<sesso>$sesso</sesso>\n<giorno>$giorno</giorno>\n<mese>$mese</mese>\n<anno>$anno</anno>\n</utente>";
 my $new_nodo = $parser->parse_balanced_chunk($new_nodo_string);
 my $root->appendChild($new_nodo);
 print OUT $doc->toString;
