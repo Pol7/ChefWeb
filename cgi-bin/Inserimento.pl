1f37bbdb45d4b398f404de3914a6d7b94d429a15
pozz92 <andrea.pozzato7@virgilio.it>
2014-03-11 11:29:57 +0100

    prova

--- cgi-bin/Inserimento.pl
+++ cgi-bin/Inserimento.pl
@@ -10,7 +10,7 @@ my $pagina = new CGI;
 
 my $file = '../public_html/database/ricette.xml';
 #creazione oggetto parser
-my $parser = XML::LibXML->new();
+#my $parser = XML::LibXML->new();
 #apertura file e lettura input
 #my $doc = $parser->parse_file($file) || die("Operazioni di parsing fallita");
 #recupero l'elemento radice
@@ -19,11 +19,11 @@ my $parser = XML::LibXML->new();
 #recupero input della form
 #my $in = $ENV{'QUERY_STRING'};
 
-my $nome = $pagina->param('nomeRicetta');
-my $tipo = $pagina->param('tipologia');
-my $autore = $pagina->param('nomeAutore');
-my $img = $pagina->param('immagine');
-my $proc = $pagina->param('message');
+#my $nome = $pagina->param('nomeRicetta');
+#my $tipo = $pagina->param('tipologia');
+#my $autore = $pagina->param('nomeAutore');
+#my $img = $pagina->param('immagine');
+#my $proc = $pagina->param('message');
 
 #my $new_nodo_string = "<ricetta tipo='$tipo'>\n<nome>$nome</nome>\n<autore>$autore</autore>\n<img></img>\n<procedimento>$proc</procedimento>\n</ricetta>";
 #my $new_nodo = $parser->parse_balanced_chunk($new_nodo_string);
@@ -31,7 +31,7 @@ my $proc = $pagina->param('message');
 
 
 
-
+print "ciao";
 
 
 #creo una stringa con un nuovo elemento
