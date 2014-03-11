71fe10e7b5dc991391de8a0231ca46ae58b9cf20
pozz92 <andrea.pozzato7@virgilio.it>
2014-03-11 11:28:35 +0100

    prova

--- cgi-bin/Inserimento.pl
+++ cgi-bin/Inserimento.pl
@@ -12,12 +12,12 @@ my $file = '../public_html/database/ricette.xml';
 #creazione oggetto parser
 my $parser = XML::LibXML->new();
 #apertura file e lettura input
-my $doc = $parser->parse_file($file) || die("Operazioni di parsing fallita");
+#my $doc = $parser->parse_file($file) || die("Operazioni di parsing fallita");
 #recupero l'elemento radice
-my $root = $doc->getDocumentElement || die("Non accedo alla radice");
+#my $root = $doc->getDocumentElement || die("Non accedo alla radice");
 
 #recupero input della form
-my $in = $ENV{'QUERY_STRING'};
+#my $in = $ENV{'QUERY_STRING'};
 
 my $nome = $pagina->param('nomeRicetta');
 my $tipo = $pagina->param('tipologia');
@@ -25,7 +25,6 @@ my $autore = $pagina->param('nomeAutore');
 my $img = $pagina->param('immagine');
 my $proc = $pagina->param('message');
 
-print $nome;
 #my $new_nodo_string = "<ricetta tipo='$tipo'>\n<nome>$nome</nome>\n<autore>$autore</autore>\n<img></img>\n<procedimento>$proc</procedimento>\n</ricetta>";
 #my $new_nodo = $parser->parse_balanced_chunk($new_nodo_string);
 #my $ricetta_to_insert->appendChild($new_nodo);
