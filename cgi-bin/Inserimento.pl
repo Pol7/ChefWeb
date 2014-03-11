b1702746d8d7ad5d6ba4ae1855849b1f5ff20036
Calu92 <luca.favaretto@hotmail.it>
2014-03-11 14:45:59 +0100

    modifica css

--- cgi-bin/Inserimento.pl
+++ cgi-bin/Inserimento.pl
@@ -43,8 +43,8 @@ print '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.o
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Ricette</title>
 <link href="../css/page_style.css" rel="stylesheet" type="text/css" media="screen"/>
-<link href="./css/page_styleMedium.css" rel="stylesheet" type="text/css" media="handheld, screen and (max-width:1320px), only screen and (max-device-width:1320px)" />
-<link href="./css/page_styleSmall.css" rel="stylesheet" type="text/css" media="handheld, screen and (max-width:690px), only screen and (max-device-width:690px)" />
+<link href="../css/page_styleMedium.css" rel="stylesheet" type="text/css" media="handheld, screen and (max-width:1320px), only screen and (max-device-width:1320px)" />
+<link href="../css/page_styleSmall.css" rel="stylesheet" type="text/css" media="handheld, screen and (max-width:690px), only screen and (max-device-width:690px)" />
 <script type="text/javascript" src="./javascript/formR.js"></script>
 </head>';
 print '	<body>
