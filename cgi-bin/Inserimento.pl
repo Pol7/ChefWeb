9d283159ae8e28653f194901fc1522c93b8b93f3
Calu92 <luca.favaretto@hotmail.it>
2014-03-11 15:09:44 +0100

    prova

--- cgi-bin/Inserimento.pl
+++ cgi-bin/Inserimento.pl
@@ -8,34 +8,33 @@ use warnings;
 
 my $pagina = new CGI;
 
-#my $file = '../public_html/database/ricette.xml';
+my $file = '../public_html/database/ricette.xml';
 #creazione oggetto parser
-#my $parser = XML::LibXML->new();
+my $parser = XML::LibXML->new();
 #apertura file e lettura input
-#my $doc = $parser->parse_file($file) || die("Operazioni di parsing fallita");
+my $doc = $parser->parse_file($file) || die("Operazioni di parsing fallita");
 #recupero l'elemento radice
-#my $root = $doc->getDocumentElement || die("Non accedo alla radice");
+my $root = $doc->getDocumentElement || die("Non accedo alla radice");
 
 #recupero input della form
-#my $in = $ENV{'QUERY_STRING'};
+my $in = $ENV{'QUERY_STRING'};
 
-#my $nome = $pagina->param('nomeRicetta');
-#my $tipo = $pagina->param('tipologia');
-#my $autore = $pagina->param('nomeAutore');
-#my $img = $pagina->param('immagine');
-#my $proc = $pagina->param('message');
+my $nome = $pagina->param('nomeRicetta');
+my $tipo = $pagina->param('tipologia');
+my $autore = $pagina->param('nomeAutore');
+my $img = $pagina->param('immagine');
+my $proc = $pagina->param('message');
 
-#my $new_nodo_string = "<ricetta tipo='$tipo'>\n<nome>$nome</nome>\n<autore>$autore</autore>\n<img></img>\n<procedimento>$proc</procedimento>\n</ricetta>";
-#my $new_nodo = $parser->parse_balanced_chunk($new_nodo_string);
-#my $ricetta_to_insert->appendChild($new_nodo);
-
-
+my $new_nodo_string = "<ricetta tipo='$tipo'>\n<nome>$nome</nome>\n<autore>$autore</autore>\n<img></img>\n<procedimento>$proc</procedimento>\n</ricetta>";
+my $new_nodo = $parser->parse_balanced_chunk($new_nodo_string);
+my $ricetta_to_insert->appendChild($new_nodo);
 
 
-#creo una stringa con un nuovo elemento
-#$nuovo_el = "\n<ricetta> $input{'contenuto'}</ricetta>\n";
 
 
+
+
+#pagina HTML
 print "Content-type: text/html\n\n";
 print '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
