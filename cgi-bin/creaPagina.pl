ee9a877bbfe351dac01b8e39293afc58cf887b36
Pol7 <Paolo@Paolos-MacBook-Pro.local>
2014-03-11 15:08:46 +0100

    prova

--- cgi-bin/creaPagina.pl
+++ cgi-bin/creaPagina.pl
@@ -97,6 +97,7 @@ for my $node ($doc->findnodes("//ricetta[\@tipo=\"$tipo\"]")){
 }
 
 sub cerca(){
+	print 'merda';
 	for my $node ($doc->findnodes("//ricetta[nome='Antipasto']")->get_node(1)){
 				print 'merda';
 	}
