41fcfad2d3f36ddfa51fbe4291acf63f82231f61
Manu11592 <emanuelezorzi_manu@hotmail.it>
2014-03-11 14:23:36 +0100

    ciao

--- cgi-bin/visualizzaRicetta.pl
+++ cgi-bin/visualizzaRicetta.pl
@@ -51,7 +51,7 @@ print ' <div id="header">
         #"//book[isbn = ‘12345']/pages/text()";
         #my $node = $xmlEvents->findnodes("/ns:events/ns:event[\@id=\"$id\"]")->get_node(0);
         for my $node ($doc->findnodes("//ricetta/nome=\"$nome\"")){
-				print "$node";
+				print "ciao";
 		}
 
 
