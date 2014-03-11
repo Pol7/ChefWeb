dfc7407cd888c2beb391c960fba19cf119d659bc
pozz92 <andrea.pozzato7@virgilio.it>
2014-03-11 12:08:17 +0100

    prova

--- cgi-bin/Inserimento.pl
+++ cgi-bin/Inserimento.pl
@@ -36,18 +36,45 @@ my $pagina = new CGI;
 #$nuovo_el = "\n<ricetta> $input{'contenuto'}</ricetta>\n";
 
 #codice per visualizzare alcuni
-print "Content-type: text/html\n\n";
-print "<html> <head><title> Pagina riassuntiva</title></head>";
-print "<body>";
-my $in = $ENV{'QUERY_STRING'};
-my $metodo = $ENV{'REQUEST_METHOD'};
-print "Query string : <b>$in</b><br>";
-print "Request method: <b>$metodo</b><br>";
-print "</body></html>";
+#print "Content-type: text/html\n\n";
+#print "<html> <head><title> Pagina riassuntiva</title></head>";
+#print "<body>";
+#my $in = $ENV{'QUERY_STRING'};
+#my $metodo = $ENV{'REQUEST_METHOD'};
+#print "Query string : <b>$in</b><br>";
+#print "Request method: <b>$metodo</b><br>";
+#print "</body></html>";
 
+print '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">/n<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><title>Ricette</title><link href="./css/page_style.css" rel="stylesheet" type="text/css" media="screen"/><link href="./css/page_styleMedium.css" rel="stylesheet" type="text/css" media="handheld, screen and (max-width:1320px), only screen and (max-device-width:1320px)" /><link href="./css/page_styleSmall.css" rel="stylesheet" type="text/css" media="handheld, screen and (max-width:690px), only screen and (max-device-width:690px)" /><script type="text/javascript" src="./javascript/formR.js"></script></head>'
+print '	<body>
+<div id="header">
+<div id="register">
+<a href="Registrazione.html">Accedi!</a>
+</div>
+</div>
+<div id="sottoHeader">
+<div id="path"> Ti trovi in: <a id="linkPercorso" href="index.html" xml:lang="en">Home</a> > Inserisci Ricetta</div>
+<input class="search" type="submit" value="Cerca!">
+<input class="search" type="text" name="Cerca:" value="" placeholder="Ricerca">
+</div>
+<div id="menu">
+<ul>
+<li class="listMenu"><a class="listMenu2" href="index.html" xml:lang="en">Home</a></li>
+<li class="listMenu"><a class="listMenu2" href="./cgi-bin/creaPagina.pl?tipo=Primi">Primi</a></li>
+<li class="listMenu"><a class="listMenu2" href="./cgi-bin/creaPagina.pl?tipo=Secondi">Secondi</a></li>
+<li class="listMenu"><a class="listMenu2" href="./cgi-bin/creaPagina.pl?tipo=Contorni">Contorni</a></li>
+<li class="listMenu"><a class="listMenu2" href="./cgi-bin/creaPagina.pl?tipo=Antipasti">Antipasti</a></li>
+<li class="listMenu"><a class="listMenu2" href="./cgi-bin/creaPagina.pl?tipo=Cocktail">Cocktail</a></li>
+<li class="listMenu"><a class="listMenu2" href="./cgi-bin/creaPagina.pl?tipo=Dessert">Dessert</a></li>
+<li class="listMenu"><a class="listMenu2" >Inserisci Ricetta</a></li>
+</ul>
+<div id="clearBoth"></div>
+</div>
+
+<div id="maincol">'
 my $nome = $pagina->param('nomeRicetta');
 print "$nome"
-
+print '</div>'
 
 
 
