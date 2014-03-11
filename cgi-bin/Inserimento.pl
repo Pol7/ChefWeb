934e765af3742b8df1fe6fcfe6f5f6aaeb6e7ba3
pozz92 <andrea.pozzato7@virgilio.it>
2014-03-11 11:48:27 +0100

    asd

--- cgi-bin/Inserimento.pl
+++ cgi-bin/Inserimento.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 # Script che inserisce i dati nell'XML
-use CGI;
+#use CGI;
 #use CGI::Carp qw(fatalsToBrowser);
-use XML::LibXML;
+#use XML::LibXML;
 #use strict;
 #use warnings;
 
@@ -31,13 +31,20 @@ use XML::LibXML;
 
 
 
-print 'ciao';
-
 
 #creo una stringa con un nuovo elemento
 #$nuovo_el = "\n<ricetta> $input{'contenuto'}</ricetta>\n";
 
-
+#!/usr/bin/perl
+#codice per visualizzare alcuni
+print "Content-type: text/html\n\n";
+print "<html> <head><title> Pagina riassuntiva</title></head>";
+print "<body>";
+$in = $ENV{'QUERY_STRING'};
+$metodo = $ENV{'REQUEST_METHOD'};
+print "Query string : <b>$in</b><br>";
+print "Request method: <b>$metodo</b><br>";
+print "</body></html>";
 
 
 
