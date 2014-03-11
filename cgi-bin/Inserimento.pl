3d986eecd03016b5d47651484d7e9096a04df44c
Calu92 <luca.favaretto@hotmail.it>
2014-03-11 15:05:37 +0100

    modifica title

--- cgi-bin/Inserimento.pl
+++ cgi-bin/Inserimento.pl
@@ -41,7 +41,7 @@ print '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.o
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Ricette</title>
+<title>Ricetta inserita</title>
 <link href="../css/page_style.css" rel="stylesheet" type="text/css" media="screen"/>
 <link href="../css/page_styleMedium.css" rel="stylesheet" type="text/css" media="handheld, screen and (max-width:1320px), only screen and (max-device-width:1320px)" />
 <link href="../css/page_styleSmall.css" rel="stylesheet" type="text/css" media="handheld, screen and (max-width:690px), only screen and (max-device-width:690px)" />
