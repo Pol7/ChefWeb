7c6f824d42a8ee8f92f0c9782288c7c7a5226abd
Manu11592 <emanuelezorzi_manu@hotmail.it>
2014-03-12 23:21:29 +0100

    Update visualizzaRicetta.pl

--- cgi-bin/visualizzaRicetta.pl
+++ cgi-bin/visualizzaRicetta.pl
@@ -54,9 +54,8 @@ print ' <div id="header">
         		<h1>'.$node->find('./nome')->get_node(0).'</h1>
         	  	<p>Autore: '.$node->find('./autore')->get_node(0).'</p>
         	  	<p>Ingrediente: '.$node->find('./ingrediente')->get_node(0).'</p>
-        	  	<div class="immagine">
-				<img src=../images/ricette/'.$node->find('./img/@src').' class="immagineVisualizzaRicetta"  alt="immagine rappresentativa della ricetta"/>
-			</div>';
+        	  	<img src=../images/ricette/'.$node->find('./img/@src').' class="immagineVisualizzaRicetta"  alt="immagine rappresentativa della ricetta"/>
+			';
 		
 			if($node->find('./ingrediente')){
 			 print 'ca';
