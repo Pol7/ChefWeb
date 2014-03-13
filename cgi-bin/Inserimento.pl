15ca0d91e89ff81b3b10a7b774c8ba750538e5dc
pozz92 <andrea.pozzato7@virgilio.it>
2014-03-13 21:40:03 +0100

    ins ing completato

--- cgi-bin/Inserimento.pl
+++ cgi-bin/Inserimento.pl
@@ -100,8 +100,7 @@ print '	<body>
 
 <div id="maincol">';
 print '     <div id="testo"> 
-            <h1 id="testo1">Ricetta inserita correttamente</h1>';
-print "@ingrediente , @quantita, @unita, $frammento2,$i<br/>";        
+            <h1 id="testo1">Ricetta inserita correttamente</h1>';    
 print "$doc"; 
 print '     <p class="testo2"> Torna alla <a href="../index.html" xml:lang="en"> Home</a></p>
             <p class="testo2"> Inserisci una <a href="../formRicette.html">Nuova Ricetta</a></p> 
