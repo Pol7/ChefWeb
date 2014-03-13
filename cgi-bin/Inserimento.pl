18bdced5db582c4fce668499bfeefdba270361b2
pozz92 <andrea.pozzato7@virgilio.it>
2014-03-13 21:24:26 +0100

    prova

--- cgi-bin/Inserimento.pl
+++ cgi-bin/Inserimento.pl
@@ -101,7 +101,7 @@ print '	<body>
 <div id="maincol">';
 print '     <div id="testo"> 
             <h1 id="testo1">Ricetta inserita correttament<h1>';
-print "@ingrediente , @quantita, @unita<br/>";        
+print "@ingrediente , @quantita, @unita, $frammento2<br/>";        
 print "$doc"; 
 print '     <p class="testo2"> Torna alla <a href="../index.html" xml:lang="en"> Home</a></p>
             <p class="testo2"> Inserisci una <a href="../formRicette.html">Nuova Ricetta</a></p> 
