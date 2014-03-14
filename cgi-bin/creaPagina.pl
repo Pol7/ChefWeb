42502fd19a3cd683e80031700057d21d1a3bc03b
Calu92 <luca.favaretto@hotmail.it>
2014-03-14 15:42:47 +0100

    aggiunta favicon

--- cgi-bin/creaPagina.pl
+++ cgi-bin/creaPagina.pl
@@ -35,7 +35,7 @@ print $pagina->start_html(
 						  { -media => 'handheld, screen and (max-width:690px), only screen and (max-device-width:690px)',
 							-src => '../css/page_styleSmall.css'}],	
 				-lang=>'it',
-				-head=>$q->Link({ -rel=>'shortcut icon', 
+				-head=>my $q->Link({ -rel=>'shortcut icon', 
 						  -href => '../images/chef.ico', 
 						  -type => 'image/x-icon'})				
 		);
