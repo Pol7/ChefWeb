0054731fc6eeef9f0531bb056624ada02c871d6a
Pol7 <paolostefani92@gmail.com>
2014-03-20 15:41:34 +0100

    Update visualizzaRicetta.pl

--- cgi-bin/visualizzaRicetta.pl
+++ cgi-bin/visualizzaRicetta.pl
@@ -86,7 +86,7 @@ print	'	<div id="clearBoth"></div>
 					<img src="../images/valid-xhtml10.png" alt="CSS Valid!"/></a>
 			</div>
 			<div id="footerText">
-			Gruppo beo
+			Statistic Chef
 			</div>
 			<div id="footerImg2">
 				<a href="http://jigsaw.w3.org/css-validator/" tabindex="13">
