55e325d5e143463fa0600f2e1de49d44a19d2199
Pol7 <Paolo@paolos-mbp.homenet.telecomitalia.it>
2014-03-22 15:46:14 +0100

    ris

--- cgi-bin/creaPagina.pl
+++ cgi-bin/creaPagina.pl
@@ -133,6 +133,7 @@ print $pagina->end_html;
 sub pasti(){
     my $i=0;		#variabile che indicare quanti cicli fa il for, serve per limitare il numero di ricette per pagina
 	for my $node ($doc->findnodes("//ricetta[\@tipo=\"$tipo\"]")){
+        $i++;
         #esce dal ciclo quando raggiunge il limite
         if($i==(10*($pag+1))){
           last;
@@ -141,7 +142,6 @@ sub pasti(){
         if($i> (10*($pag))){
  	         elencoRicette($node);
         }
-        $i++;
 	}
     if($i==10*($pag+1)){
         $pag++;
